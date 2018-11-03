66259b2433f4a4caaea0eef5602f18a8e164f7a4
woodser <woodser@protonmail.com>
2018-11-03 10:20:34 -0400

    lesson in es6+

--- local_modules/MainWindow/MoneroWalletMM.js
+++ local_modules/MainWindow/MoneroWalletMM.js
@@ -26,56 +26,137 @@ class MoneroWalletMM {
     throw "getHeight() not implemented";
   }
   
-  sync() {
+  async sync() {
+   
+
     
-    /*
-     * https://stackoverflow.com/questions/24586110/resolve-promises-one-after-another-i-e-in-sequence
-     * 
-     * serial executes Promises sequentially.
-     * @param {funcs} An array of funcs that return promises.
-     * @example
-     * const urls = ['/url1', '/url2', '/url3']
-     * serial(urls.map(url => () => $.ajax(url)))
-     *     .then(console.log.bind(console))
-     */
-    const serial = funcs =>
-        funcs.reduce((promise, func) =>
-            promise.then(result => func().then(Array.prototype.concat.bind(result))), Promise.resolve([]))
+    // get height
+    let resp = await this.daemon.get_height();
     
-    this.daemon.get_height().then(resp => {
-      console.log("Height: " + resp.height);
-      
-      const NUM_BLOCKS = 10;
-      let endHeight = resp.height - 1;
-      let startHeight = endHeight - NUM_BLOCKS + 1;
-      console.log("Getting blocks from range: [" + startHeight + ", " + endHeight + "]");
-      this.daemon.get_block_headers_range(startHeight, endHeight)
-        .then(headersResp => {
-          
-          // fetch blocks
-          let requests = headersResp.headers.map(header => () => this.daemon.getblock_by_height(header.height));
-          serial(requests).then(blocks => {
-            
-            // collect transaction hashes
-            let txHashes = [];
-            for (let block of blocks) {
-              console.log(block);
-              if (block.tx_hashes === undefined) continue;
-              for (let txHash of block.tx_hashes) {
-                txHashes.push(txHash);
-              }
-            }
-            
-            // fetch transactions
-            console.log(txHashes);
-          }).catch(err => {
-            console.log(err);
-          })
-        })
-        .catch(errResp => {
-          console.log("Error get headers range! " + errResp);
-        });
-    });
+    // fetch block headers
+    const NUM_BLOCKS = 10;
+    let endHeight = resp.height - 1;
+    let startHeight = endHeight - NUM_BLOCKS + 1;
+    console.log("Getting blocks from range: [" + startHeight + ", " + endHeight + "]");
+    let headersResp = await this.daemon.get_block_headers_range(startHeight, endHeight);
+    
+    // fetch blocks
+    let requests = headersResp.headers.map(header => () => this.daemon.getblock_by_height(header.height));
+    let blocks = [];
+    for (let request of requests) {
+      blocks.push(await request());
+    }
+    
+    // collect transaction hashes
+//    let txHashes = blocks.map(block => block.tx_hashes === undefined ? [] : block.tx_hashes).reduce((a, b) => a.concat(b)); // works but bad memory profile
+//    let txHashes = blocks.map(block => block.tx_hashes === undefined ? [] : block.tx_hashes).reduce((a, b) => { a.push.apply(a, b); return a; }); // works
+    
+    let txHashes = [];
+    for (let block of blocks) {
+      if (block.tx_hashes === undefined) continue;
+      for (let txHash of block.tx_hashes) {
+        txHashes.push(txHash);
+      }
+    }
+    
+    console.log(txHashes);
+    
+    
+    
+//    let txHashes = [["1"], ["2", "3"]].reduce((a, b) => a.concat(b), []);
+//    console.log(txHashes);
+    //blocks.map(block => block.tx_hashes === undefined ? [] : block.tx_hashes)
+    
+    
+    
+    
+
+    
+//    console.log(requests);
+//    for (let request of requests) {
+//      console.log(request instanceof Promise);
+//      request.then(function() {
+//        console.log("request RESOLVED!");
+//      }).catch(function() {
+//        console.log("request FAILED");
+//      });
+//    }
+    
+    
+    //console.log(await requests[0]);
+    
+//    for (let request of requests) {
+//      console.log(requests);
+//      let block = await request;
+//      console.log(block);
+//    }
+    
+//    const serial = funcs =>
+//      funcs.reduce((promise, func) =>
+//          promise.then(result => func().then(Array.prototype.concat.bind(result))), Promise.resolve([]))
+//    serial(requests).then(blocks => {
+//      console.log(blocks);
+//      
+
+//      
+//      // fetch transactions
+//      console.log(txHashes);
+//    });
+    
+    
+//    let blocks = await Promise.all(requests);
+//    console.log(blocks);
+//    
+//    // collect transaction hashes
+//    let txHashes = [];
+//    for (let block of blocks) {
+//      if (block.tx_hashes === undefined) continue;
+//      for (let txHash of block.tx_hashes) {
+//        txHashes.push(txHash);
+//      }
+//    }
+//    
+//    // fetch transactions
+//    console.log(txHashes);
+    
+
+//    
+//    this.daemon.get_height().then(resp => {
+//      console.log("Height: " + resp.height);
+//      
+//      const NUM_BLOCKS = 10;
+//      let endHeight = resp.height - 1;
+//      let startHeight = endHeight - NUM_BLOCKS + 1;
+//      console.log("Getting blocks from range: [" + startHeight + ", " + endHeight + "]");
+//      this.daemon.get_block_headers_range(startHeight, endHeight)
+//        .then(headersResp => {
+//          
+//          // fetch blocks
+//          let requests = headersResp.headers.map(header => () => this.daemon.getblock_by_height(header.height));
+//          console.log(requests);
+//          serial(requests).then(blocks => {
+//            console.log(blocks);
+//            
+//            // collect transaction hashes
+//            let txHashes = [];
+//            for (let block of blocks) {
+//              console.log(block);
+//              if (block.tx_hashes === undefined) continue;
+//              for (let txHash of block.tx_hashes) {
+//                txHashes.push(txHash);
+//              }
+//            }
+//            
+//            // fetch transactions
+//            console.log(txHashes);
+//          }).catch(err => {
+//            console.log(err);
+//          })
+//        })
+//        .catch(errResp => {
+//          console.log("Error get headers range! " + errResp);
+//        });
+//    });
   }
 }
 
