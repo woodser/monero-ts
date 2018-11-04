b4558c2da98329d0796b68abd23c10d7d6ae7984
woodser <woodser@protonmail.com>
2018-11-03 20:00:34 -0400

    seed that actually works (?)

--- local_modules/MainWindow/index.js
+++ local_modules/MainWindow/index.js
@@ -12,9 +12,11 @@ window.BootApp = function() {
 	  
 	  // seed for testing or undefined
 	  //let seed = undefined;
-	  let seed = "mohawk veteran buffet siren pencil pause worry aggravate upright chrome aptitude roomy nibs extra oozed irony tonic gusts aplomb nostril update heron reunion sadness chrome";
-	  let primaryAddress = "51sLpF8fWaK11111111111111111111111111111111112N1GuTZeagfRbbKcALdcZev4QXGGuoLh2x36LhaxLSxCZiDpfU";  // just for reference
-	  
+	  //let seed = "mohawk veteran buffet siren pencil pause worry aggravate upright chrome aptitude roomy nibs extra oozed irony tonic gusts aplomb nostril update heron reunion sadness chrome";
+	  //let primaryAddress = "51sLpF8fWaK11111111111111111111111111111111112N1GuTZeagfRbbKcALdcZev4QXGGuoLh2x36LhaxLSxCZiDpfU";  // just for reference
+	  let seed = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
+	  let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
+
     new MoneroRPC.daemonRPC({ hostname: "localhost", port: 38081, user: "superuser", pass: "abctesting123", protocol: "http", network: "stagenet" })
 //	  new MoneroRPC.daemonRPC("localhost", 38081, "superuser", "abctesting123", "http", "mainnet")
 //    new MoneroRPC.daemonRPC({ autoconnect: true, random: true, user: "superuser", pass: "abctesting123" })
