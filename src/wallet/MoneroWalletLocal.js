9bcc72573a8b8f19919e1d40c62851ea69a85d16
woodser <woodser@protonmail.com>
2018-11-06 22:09:47 -0500

    ready to implement getBlocks

--- src/wallet/MoneroWalletLocal.js
+++ src/wallet/MoneroWalletLocal.js
@@ -72,81 +72,89 @@ class MoneroWalletLocal extends MoneroWallet {
     let headers = await this.daemon.getBlockHeaders(startHeight, endHeight);
     
     // fetch blocks
-    let requests = headers.map(header => () => this.daemon.getblock_by_height(header.getHeight()));
-    let blocks = [];
-    for (let request of requests) {
-      blocks.push(await request());
-    }
+    let heights = headers.map(header => header.getHeight());
+    let blocks = await this.daemon.getBlocks(heights);
     
-    // collect transaction hashes
-//    let txHashes = blocks.map(block => block.tx_hashes === undefined ? [] : block.tx_hashes).reduce((a, b) => a.concat(b)); // works but bad memory profile
-    let txHashes = blocks.map(block => block.tx_hashes === undefined ? [] : block.tx_hashes).reduce((a, b) => { a.push.apply(a, b); return a; }); // works
-//    let txHashes = [];
-//    for (let block of blocks) {
-//      if (block.tx_hashes === undefined) continue;
-//      for (let txHash of block.tx_hashes) {
-//        txHashes.push(txHash);
+    
+    
+    
+    
+//    // fetch blocks
+//    let requests = headers.map(header => () => this.daemon.getblock_by_height(header.getHeight()));
+//    let blocks = [];
+//    for (let request of requests) {
+//      blocks.push(await request());
+//    }
+//    
+//    // collect transaction hashes
+////    let txHashes = blocks.map(block => block.tx_hashes === undefined ? [] : block.tx_hashes).reduce((a, b) => a.concat(b)); // works but bad memory profile
+//    let txHashes = blocks.map(block => block.tx_hashes === undefined ? [] : block.tx_hashes).reduce((a, b) => { a.push.apply(a, b); return a; }); // works
+////    let txHashes = [];
+////    for (let block of blocks) {
+////      if (block.tx_hashes === undefined) continue;
+////      for (let txHash of block.tx_hashes) {
+////        txHashes.push(txHash);
+////      }
+////    }
+//    
+//    // fetch transactions
+//    let txResp = await this.daemon.get_transactions(txHashes, true, false);
+//    let txs = txResp.txs_as_json.map(txStr => JSON.parse(txStr));
+//    if (txHashes.length !== txs.length) throw new Error("Missing fetched transactions");
+//    
+//    // process transactions
+//    //let decoder = new TextDecoder("utf-8");
+//    let numOwned = 0;
+//    let numUnowned = 0;
+//    
+//    console.log("View key prv: " + this.viewKeyPrv);
+//    console.log("Spend key pub: " + this.spendKeyPub);
+//    
+//    console.log("Processing transactions...");
+//    for (let txIdx = 0; txIdx < txHashes.length; txIdx++) {
+//      let tx = txs[txIdx];
+//      if (txHashes[txIdx] !== "cb8258a925b63a43f4447fd3c838b0d5b9389d1df1cd4c6e18b0476d2c221c9f") continue;
+//      
+//      console.log("Tx hash: " + txHashes[txIdx]);
+//      console.log(tx);
+//      
+//      // get tx pub key
+//      let lastPubKey;
+//      try {
+//        lastPubKey = MoneroUtils.getLastTxPubKey(tx.extra);
+//        console.log("Last pub key: " + lastPubKey);
+//      } catch (err) {
+//        console.log("Could not process nonstandard extra: " + tx.extra);
+//        continue;
+//      }
+//      
+//      // process outputs
+//      for (let idx = 0; idx < tx.vout.length; idx++) {
+//        let out = tx.vout[idx];
+//        //let pubKey = out.target.key;
+//        
+//
+////        console.log("Amount: " + amount);
+//        console.log("Index: " + idx);
+//        
+//        let derivation = this.coreUtils.generate_key_derivation(lastPubKey, this.viewKeyPrv);
+//        console.log("Derivation: " + derivation);
+//        
+//        let pubKeyDerived = this.coreUtils.derive_public_key(derivation, idx, this.spendKeyPub);
+//        console.log("Pub key derived: " + pubKeyDerived);
+//        
+//        // check if wallet owns output
+//        if (lastPubKey === pubKeyDerived) {
+//          console.log("This my output!!!");
+//          numOwned++;
+//          console.log(out);
+//          
+//          // TODO: determine amount and test
+//        } else {
+//          numUnowned++;
+//        }
 //      }
 //    }
-    
-    // fetch transactions
-    let txResp = await this.daemon.get_transactions(txHashes, true, false);
-    let txs = txResp.txs_as_json.map(txStr => JSON.parse(txStr));
-    if (txHashes.length !== txs.length) throw new Error("Missing fetched transactions");
-    
-    // process transactions
-    //let decoder = new TextDecoder("utf-8");
-    let numOwned = 0;
-    let numUnowned = 0;
-    
-    console.log("View key prv: " + this.viewKeyPrv);
-    console.log("Spend key pub: " + this.spendKeyPub);
-    
-    console.log("Processing transactions...");
-    for (let txIdx = 0; txIdx < txHashes.length; txIdx++) {
-      let tx = txs[txIdx];
-      if (txHashes[txIdx] !== "cb8258a925b63a43f4447fd3c838b0d5b9389d1df1cd4c6e18b0476d2c221c9f") continue;
-      
-      console.log("Tx hash: " + txHashes[txIdx]);
-      console.log(tx);
-      
-      // get tx pub key
-      let lastPubKey;
-      try {
-        lastPubKey = MoneroUtils.getLastTxPubKey(tx.extra);
-        console.log("Last pub key: " + lastPubKey);
-      } catch (err) {
-        console.log("Could not process nonstandard extra: " + tx.extra);
-        continue;
-      }
-      
-      // process outputs
-      for (let idx = 0; idx < tx.vout.length; idx++) {
-        let out = tx.vout[idx];
-        //let pubKey = out.target.key;
-        
-
-//        console.log("Amount: " + amount);
-        console.log("Index: " + idx);
-        
-        let derivation = this.coreUtils.generate_key_derivation(lastPubKey, this.viewKeyPrv);
-        console.log("Derivation: " + derivation);
-        
-        let pubKeyDerived = this.coreUtils.derive_public_key(derivation, idx, this.spendKeyPub);
-        console.log("Pub key derived: " + pubKeyDerived);
-        
-        // check if wallet owns output
-        if (lastPubKey === pubKeyDerived) {
-          console.log("This my output!!!");
-          numOwned++;
-          console.log(out);
-          
-          // TODO: determine amount and test
-        } else {
-          numUnowned++;
-        }
-      }
-    }
     
     console.log("Done processing, " + numOwned + " owned outputs found, " + numUnowned + " unowned");
   }
