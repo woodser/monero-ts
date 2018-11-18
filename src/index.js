58c160fc7e17e0ef727118291e037b5a5aaf9e5f
woodser <woodser@protonmail.com>
2018-11-18 09:35:20 -0500

    update index.js for to run in browser

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 const MoneroDaemonRpc = require("./daemon/MoneroDaemonRpc");
 const MoneroWalletLocal = require("./wallet/MoneroWalletLocal");
 
-const MoneroRPC = require("./monerojs");
 //const WalletHostPollingController = require('../Wallets/Controllers/WalletHostPollingController')
 
 console.log("Booting app...");
@@ -18,8 +17,8 @@ require('./submodules/mymonero-core-js/monero_utils/monero_utils')().then(functi
   let mnemonic = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
   let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
   
-  let daemonRpc = new MoneroDaemonRpc({ port: 38081, user: "superuser", pass: "abctesting123", protocol: "http" });
+  let daemonRpc = new MoneroDaemonRpc({ port: 38081, user: "superuser", pass: "abctesting123", protocol: "http", coreUtils: coreUtils });
   let wallet = new MoneroWalletLocal(daemonRpc, coreUtils, mnemonic);
   if (primaryAddress !== wallet.getPrimaryAddress()) throw "Addresses do not match";
   wallet.sync();
-})
+});
