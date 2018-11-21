f608955bc5fcc47abf811d04f11ebba654a79f8f
woodser <woodser@protonmail.com>
2018-11-20 21:38:11 -0500

    update index.js to run in browser

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 "use strict"
 
+const MoneroUtils = require("./utils/MoneroUtils");
 const MoneroDaemonRpc = require("./daemon/MoneroDaemonRpc");
 const MoneroWalletLocal = require("./wallet/MoneroWalletLocal");
 
 //const WalletHostPollingController = require('../Wallets/Controllers/WalletHostPollingController')
 
-console.log("Booting app...");
-require('./submodules/mymonero-core-js/monero_utils/monero_utils')().then(function(coreUtils) {
-  console.log("Core utils loaded");
-  //console.log(coreUtils);
+startApp();
+async function startApp() {
+  console.log("Starting app...");
+  
+  // get core utils TODO: if daemon and wallet functions are already async could not do this instead, then again would be nice to have some functions not be async (e.g. getPrimaryAddress())
+  let coreUtils = await MoneroUtils.getCoreUtils();
+  console.log(coreUtils);
   
   // mnemonic for testing or undefined
-  //let mnemonic = undefined;
-  //let mnemonic = "mohawk veteran buffet siren pencil pause worry aggravate upright chrome aptitude roomy nibs extra oozed irony tonic gusts aplomb nostril update heron reunion sadness chrome";
-  //let primaryAddress = "51sLpF8fWaK11111111111111111111111111111111112N1GuTZeagfRbbKcALdcZev4QXGGuoLh2x36LhaxLSxCZiDpfU";  // just for reference
   let mnemonic = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
   let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
   
+  // daemon
   let daemonRpc = new MoneroDaemonRpc({ port: 38081, user: "superuser", pass: "abctesting123", protocol: "http", coreUtils: coreUtils });
-  let wallet = new MoneroWalletLocal(daemonRpc, coreUtils, mnemonic);
-  if (primaryAddress !== wallet.getPrimaryAddress()) throw "Addresses do not match";
+  
+  // make wallet with daemon and core utils
+  let wallet = new MoneroWalletLocal(daemonRpc, coreUtils, mnemonic); // TODO: {...} config
+  if (primaryAddress !== await wallet.getPrimaryAddress()) throw "Addresses do not match";
   wallet.refresh();
-});
+}
