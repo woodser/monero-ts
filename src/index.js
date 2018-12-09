bf90fdacafe7bd96aa8ff0d8f9f26c4bd51fff98
woodser <woodser@protonmail.com>
2018-12-09 11:39:30 -0500

    update index.js to make browser work

--- src/index.js
+++ src/index.js
@@ -10,18 +10,17 @@ startApp();
 async function startApp() {
   console.log("Starting app...");
   
-  // get core utils TODO: if daemon and wallet functions are already async could not do this instead, then again would be nice to have some functions not be async (e.g. getPrimaryAddress())
-  let coreUtils = await MoneroUtils.getCoreUtils();
+  // create daemon to support wallet
+  let daemon = new MoneroDaemonRpc({port: 38081, user: "superuser", pass: "abctesting123", protocol: "http"});
   
-  // mnemonic for testing or undefined
+  // create wallet with known mnemonic
   let mnemonic = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
   let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
-  
-  // daemon
-  let daemonRpc = new MoneroDaemonRpc({ port: 38081, user: "superuser", pass: "abctesting123", protocol: "http", coreUtils: coreUtils });
-  
-  // make wallet with daemon and core utils
-  let wallet = new MoneroWalletLocal({daemon: daemonRpc, coreUtils: coreUtils, mnemonic: mnemonic });
+  let wallet = new MoneroWalletLocal({daemon: daemon, mnemonic: mnemonic});
   if (primaryAddress !== await wallet.getPrimaryAddress()) throw "Addresses do not match";
-  wallet.refresh();
+  
+  // sync the wallet
+  await wallet.sync(null, null, function(progress) {
+    //console.log(progress.percent);
+  });
 }
