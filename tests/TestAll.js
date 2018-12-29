6c3ddc8cdf13324deea190633e5d42f32c539d64
woodser <woodser@protonmail.com>
2018-12-28 23:05:08 -0500

    re-enable tests; todos

--- tests/TestAll.js
+++ tests/TestAll.js
@@ -1,9 +1,9 @@
-//require("./TestDaemonRpc");
+require("./TestDaemonRpc");
 require("./TestWalletRpc");
-//require("./TestWalletLocal");
-//require("./TestWalletEquality");
-//require("./TestBooleanSet");
-//require("./TestCoreUtils");
-//require("./TestSampleCode");
+require("./TestWalletLocal");
+require("./TestWalletEquality");
+require("./TestBooleanSet");
+require("./TestCoreUtils");
+require("./TestSampleCode");
 
 it("Initialize tests", function() {});  // necessary if no tests start synchronously
