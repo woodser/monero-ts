f0b931106dd5158492df733dd80b98b68e069004
woodser <woodser@protonmail.com>
2019-01-07 13:26:12 -0500

    cleanup

--- src/wallet/MoneroWallet.js
+++ src/wallet/MoneroWallet.js
@@ -199,7 +199,7 @@ class MoneroWallet {
    * Get wallet transactions.
    * 
    * @param {MoneroTxFilter} or {number} returns transactions that meet the
-   *        filter or transactions to/from the account index (defaults to all)
+   *        filter or transactions from/to the account index (defaults to all)
    * @param {number} returns transactions to/from this subaddress within the 
    *        given account (defaults to all)
    */
