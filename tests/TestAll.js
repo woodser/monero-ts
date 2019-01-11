b6c1bf961463ccc5b9d565105dc355248114a457
woodser <woodser@protonmail.com>
2019-01-10 22:09:08 -0500

    narrow tests

--- tests/TestAll.js
+++ tests/TestAll.js
@@ -4,37 +4,37 @@ const TestMoneroWalletLocal = require("./TestMoneroWalletLocal")
 
 //// test daemon rpc
 new TestMoneroDaemonRpc().runTests({
-  testNotifications: true
+  testNotifications: false
 });
 
 // test wallet rpc
 new TestMoneroWalletRpc().runTests({
   testNonSends: true,
   liteMode: true, // skips some lengthy tests // TODO: lengthy tests still need to be runnable but getting test timeout
-  testSends: true,
-  testResets: false,
-  testNotifications: true
-});
-
-// test wallet local
-new TestMoneroWalletLocal().runTests({
-  testNonSends: true,
   testSends: false,
   testResets: false,
   testNotifications: false
 });
 
-// test wallet equality
-require("./TestMoneroWalletEquality");
-
 // test core utils
 require("./TestMoneroCoreUtils");
 
-// test boolean set (data structure used by wallet to track progress)
-require("./TestBooleanSet");
-
-// test sample code for readme
+//test sample code for readme
 require("./TestSampleCode");
+
+//// test wallet local
+//new TestMoneroWalletLocal().runTests({
+//  testNonSends: true,
+//  testSends: false,
+//  testResets: false,
+//  testNotifications: false
+//});
+//
+//// test wallet equality
+//require("./TestMoneroWalletEquality");
+//
+//// test boolean set (data structure used by wallet to track progress)
+//require("./TestBooleanSet");
 
 //// test scratchpad
 //require("./Scratchpad");
