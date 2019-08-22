1fbadaabde6db4028a6b3c5013003a3c7a3ba1b2
woodser <woodser@protonmail.com>
2019-08-22 13:09:11 -0400

    don't implement monero_output_wallet::merge() yet

--- src/wallet/monero_wallet_model.cpp
+++ src/wallet/monero_wallet_model.cpp
@@ -566,6 +566,7 @@ namespace monero {
   }
 
   void monero_output_wallet::merge(const shared_ptr<monero_output_wallet>& self, const shared_ptr<monero_output_wallet>& other) {
+    throw runtime_error("monero_output_wallet:merge() not implemented");
     MTRACE("monero_output_wallet::merge(self, other)");
     if (this != self.get()) throw runtime_error("this != self");
     if (self == other) return;
