538a0145fcc63ead3150614ab0278559c520d03a
woodser <woodser@protonmail.com>
2019-09-01 13:23:10 -0400

    enable all tests

--- src/test/TestAll.js
+++ src/test/TestAll.js
@@ -8,22 +8,22 @@ require("./TestSampleCode");
 //test core utils
 require("./TestMoneroCppUtils");
 
-//// test daemon rpc
-//new TestMoneroDaemonRpc().runTests({
-//  liteMode: false,  // skips some lengthy but detailed tests
-//  testNonRelays: true,
-//  testRelays: true, // creates and relays outgoing txs
-//  testNotifications: true
-//});
-//
-//// test wallet rpc
-//new TestMoneroWalletRpc().runTests({
-//  liteMode: false, // skips some lengthy but detailed tests
-//  testNonRelays: true,
-//  testRelays: true,
-//  testNotifications: true,
-//  testResets: false
-//});
+// test daemon rpc
+new TestMoneroDaemonRpc().runTests({
+  liteMode: false,  // skips some lengthy but detailed tests
+  testNonRelays: true,
+  testRelays: true, // creates and relays outgoing txs
+  testNotifications: true
+});
+
+// test wallet rpc
+new TestMoneroWalletRpc().runTests({
+  liteMode: false, // skips some lengthy but detailed tests
+  testNonRelays: true,
+  testRelays: true,
+  testNotifications: true,
+  testResets: false
+});
 
 // test wallet local
 new TestMoneroWalletLocal().runTests({
@@ -37,8 +37,8 @@ new TestMoneroWalletLocal().runTests({
 //// test wallet equality
 //require("./TestMoneroWalletEquality");
 
-//// test boolean set (data structure used by wallet to track progress)
-//require("./TestBooleanSet");
+// test boolean set (data structure used by wallet to track progress)
+require("./TestBooleanSet");
 
 // test scratchpad
-//require("./Scratchpad");
+require("./Scratchpad");
