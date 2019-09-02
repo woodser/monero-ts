1b2e3c64d812dc44d2b1ba914dbd25ffd95852d6
woodser <woodser@protonmail.com>
2019-09-02 08:04:52 -0400

    cleanup

--- src/test/utils/TestUtilsModule.js
+++ src/test/utils/TestUtilsModule.js
@@ -1,7 +1,8 @@
 /**
  * Exports test utilities for use across the tests.
  * 
- * Exporting like this resolves circular dependencies, e.g. between TestUtils and TxPoolWalletTracker.
+ * Exporting like this resolves circular dependencies when files are separate,
+ * e.g. between TestUtils and TxPoolWalletTracker.
  */
 module.exports = function() {
   this.TestUtils = require("./TestUtils");
