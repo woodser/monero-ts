8cddee1e6920e06a5785edf7a0cccec29a70f265
woodser <woodser@protonmail.com>
2019-10-04 22:01:35 -0400

    MoneroWalletWasm stores cppAddress

--- src/main/js/wallet/MoneroWalletWasm.js
+++ src/main/js/wallet/MoneroWalletWasm.js
@@ -4,14 +4,18 @@
  */
 class MoneroWalletWasm {
   
+  constructor() {
+    this.cppAddress = MoneroWalletWasm.WASM_MODULE.new_monero_wallet_dummy();
+  }
+  
   dummyMethod() {
-    return MoneroWalletWasm.WASM_MODULE.dummy_method();
+    return MoneroWalletWasm.WASM_MODULE.dummy_method(this.cppAddress);
   }
 }
 
 module.exports = async function() {
   return new Promise(function(resolve, reject) {
-    require("../../monero_cpp_library_WASM")().ready.then(function(module) {
+    require("../../../monero_cpp_library_WASM")().ready.then(function(module) {
       MoneroWalletWasm.WASM_MODULE = module;
       resolve(MoneroWalletWasm);
     }).catch(function(e) {
