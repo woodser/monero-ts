6b785bfb72d3f946cf463f33eb69ccb4edb6a672
woodser <woodser@protonmail.com>
2019-10-04 22:02:10 -0400

    index.js calls dummy_method

--- browser_app/index.js
+++ browser_app/index.js
@@ -7,31 +7,38 @@
 "use strict"
 
 // import what you want to use
-const MoneroDaemonRpc = require("../src/main/js/daemon/MoneroDaemonRpc");
-const MoneroWalletRpc = require("../src/main/js/wallet/MoneroWalletRpc");
-const MoneroWalletLocal = require("../src/main/js/wallet/MoneroWalletLocal");
+//const MoneroDaemonRpc = require("../src/main/js/daemon/MoneroDaemonRpc");
+//const MoneroWalletRpc = require("../src/main/js/wallet/MoneroWalletRpc");
+//const MoneroWalletLocal = require("../src/main/js/wallet/MoneroWalletLocal");
 
 // start the application
 startApp();
 async function startApp() {
   console.log("Starting app...");
   
-  // connect to monero-daemon-rpc
-  let daemon = new MoneroDaemonRpc({uri: "http://localhost:38081", user: "superuser", pass: "abctesting123"});
-  console.log("Daemon height: " + await daemon.getHeight());
+  // import wasm wallet
+  const MoneroWalletWasm = await require("../src/main/js/wallet/MoneroWalletWasm")();
   
-  // connect to monero-wallet-rpc
-  let walletRpc = new MoneroWalletRpc({uri: "http://localhost:38083", user: "rpc_user", pass: "abc123"});
-  console.log("Wallet rpc mnemonic: " + await walletRpc.getMnemonic());
-  console.log("Wallet rpc balance: " + await walletRpc.getBalance());
+  // demonstrate wasm wallet
+  let wallet = new MoneroWalletWasm();
+  wallet.dummyMethod();
   
-  // create a wallet from mnemonic using local wasm bindings
-  let mnemonic = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
-  let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
-  let walletLocal = new MoneroWalletLocal({daemon: daemon, mnemonic: mnemonic});
-  console.log("Local wallet address: " + await walletLocal.getPrimaryAddress());
-  console.log("Local wallet height: " + await walletLocal.getHeight());
-  if (primaryAddress !== await walletLocal.getPrimaryAddress()) throw "Addresses do not match";
+//  // connect to monero-daemon-rpc
+//  let daemon = new MoneroDaemonRpc({uri: "http://localhost:38081", user: "superuser", pass: "abctesting123"});
+//  console.log("Daemon height: " + await daemon.getHeight());
+//  
+//  // connect to monero-wallet-rpc
+//  let walletRpc = new MoneroWalletRpc({uri: "http://localhost:38083", user: "rpc_user", pass: "abc123"});
+//  console.log("Wallet rpc mnemonic: " + await walletRpc.getMnemonic());
+//  console.log("Wallet rpc balance: " + await walletRpc.getBalance());
+  
+//  // create a wallet from mnemonic using local wasm bindings
+//  let mnemonic = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
+//  let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
+//  let walletLocal = new MoneroWalletLocal({daemon: daemon, mnemonic: mnemonic});
+//  console.log("Local wallet address: " + await walletLocal.getPrimaryAddress());
+//  console.log("Local wallet height: " + await walletLocal.getHeight());
+//  if (primaryAddress !== await walletLocal.getPrimaryAddress()) throw "Addresses do not match";
   
   // sync the wallet
 //  await wallet.sync(undefined, function(progress) {
