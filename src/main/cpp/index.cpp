b4c9e9c3ca27f7ef681633dbb3cce54ff2fcf6d5
woodser <woodser@protonmail.com>
2019-10-09 14:14:39 -0400

    dummy utils method working

--- src/main/cpp/index.cpp
+++ src/main/cpp/index.cpp
@@ -9,9 +9,9 @@ EMSCRIPTEN_BINDINGS(module)
 {
   // register utility bindings
   emscripten::function("utils_dummy_method", &monero_utils_wasm_bridge::utils_dummy_method);
-  emscripten::function("malloc_binary_from_json", &monero_utils_wasm_bridge::malloc_binary_from_json);
-  emscripten::function("binary_to_json", &monero_utils_wasm_bridge::binary_to_json);
-  emscripten::function("binary_blocks_to_json", &monero_utils_wasm_bridge::binary_blocks_to_json);
+//  emscripten::function("malloc_binary_from_json", &monero_utils_wasm_bridge::malloc_binary_from_json);
+//  emscripten::function("binary_to_json", &monero_utils_wasm_bridge::binary_to_json);
+//  emscripten::function("binary_blocks_to_json", &monero_utils_wasm_bridge::binary_blocks_to_json);
 
   // register wallet bindings
   emscripten::function("create_wallet_random", &monero_wallet_wasm_bridge::create_wallet_random);
