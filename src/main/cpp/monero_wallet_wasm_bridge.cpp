24b7c929dcffeb3996e8b19c72dd403ce0025021
woodser <woodser@protonmail.com>
2019-10-29 21:48:23 -0400

    MoneroWalletWasm.createWalletRandom() returns promise which is resolved after blocking C++

--- src/main/cpp/monero_wallet_wasm_bridge.cpp
+++ src/main/cpp/monero_wallet_wasm_bridge.cpp
@@ -4,10 +4,17 @@
 #include "wallet/wallet2_base.h"
 #include "http_client_wasm.h"
 
+#include <emscripten.h> // TODO: remove
+
 using namespace std;
 using namespace monero_wallet_wasm_bridge;
 
-int monero_wallet_wasm_bridge::create_wallet_random(const string& path, const string& password, int network_type, const string& daemon_uri, const string& daemon_username, const string& daemon_password, const string& language) {
+void my_int_func(int x)
+{
+    printf( "%d\n", x );
+}
+
+int monero_wallet_wasm_bridge::create_wallet_random(const string& path, const string& password, int network_type, const string& daemon_uri, const string& daemon_username, const string& daemon_password, const string& language, emscripten::val cb) {
     cout << "create_wallet_random() called!!!" << endl;
 //  monero_rpc_connection daemon_connection = monero_rpc_connection(daemon_uri, daemon_username, daemon_password);
 //  monero_wallet* wallet = monero_wallet::create_wallet_random(path, password, static_cast<monero_network_type>(network_type), daemon_connection, language);
@@ -19,9 +26,20 @@ int monero_wallet_wasm_bridge::create_wallet_random(const string& path, const st
     w2_base->set_seed_language(language);
     cout << "Seed language: " << w2_base->get_seed_language() << endl;
     crypto::secret_key secret_key;
-    w2_base->generate(path, password, secret_key, false, false);
+
+    // simulate doing async work
+    cout << "Starting sleep" << endl;
+    emscripten_sleep(5000);
+    cout << "Done sleeping" << endl;
+
+    cout << "Calling callback" << endl;
+    cb((int) w2_base);
+    cout << "Done calling callback" << endl;
+
+    //w2_base->generate(path, password, secret_key, false, false);
 //    std::string err;
 //    if (http_client.is_connected()) w2_base->set_refresh_from_block_height(w2_base->get_daemon_blockchain_height(err));
+
     cout << "Returning new w2_base: " << ((int) w2_base) << endl;
     return (int) w2_base;
 
