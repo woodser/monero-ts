7bc39584fd8598220a3c63cf6e10d543339a4132
woodser <woodser@protonmail.com>
2019-10-30 14:55:26 -0400

    initializing daemon server and login, calling http_client::is_connected()

--- src/main/cpp/monero_wallet_wasm_bridge.cpp
+++ src/main/cpp/monero_wallet_wasm_bridge.cpp
@@ -14,6 +14,11 @@ void my_int_func(int x)
     printf( "%d\n", x );
 }
 
+// ------------------------- INITIALIZE CONSTANTS ---------------------------
+
+static const int DEFAULT_SYNC_INTERVAL_MILLIS = 1000 * 10;   // default refresh interval 10 sec
+static const int DEFAULT_CONNECTION_TIMEOUT_MILLIS = 1000 * 30; // default connection timeout 30 sec
+
 void monero_wallet_wasm_bridge::create_wallet_random(const string& path, const string& password, int network_type, const string& daemon_uri, const string& daemon_username, const string& daemon_password, const string& language, emscripten::val cb) {
     cout << "create_wallet_random() called!!!" << endl;
 //  monero_rpc_connection daemon_connection = monero_rpc_connection(daemon_uri, daemon_username, daemon_password);
@@ -23,6 +28,22 @@ void monero_wallet_wasm_bridge::create_wallet_random(const string& path, const s
     http_client_wasm http_client;
     tools::wallet2_base* w2_base = new tools::wallet2_base(http_client, static_cast<cryptonote::network_type>(network_type), 1, true);
     cout << "Created wallet2_base with address: " << ((int) w2_base) << endl;
+
+    // prepare uri, login, and is_trusted for wallet2
+    boost::optional<epee::net_utils::http::login> login{};
+    login.emplace(daemon_username, daemon_password);
+    bool is_trusted = true; // TODO: trusted iff local, but common/util is_local_address() uses boost asio
+
+    cout << "Created login" << endl;
+
+    // init wallet2 and set daemon connection
+    if (!w2_base->init(daemon_uri, login)) {
+      cout << "!!! FAILED TO INITIALIZE WALLET WITH DAEMON CONNECTION" << endl;
+      throw runtime_error("Failed to initialize wallet with daemon connection");
+    }
+    uint32_t version = 0;
+    w2_base->check_connection(&version, NULL, DEFAULT_CONNECTION_TIMEOUT_MILLIS);
+
     w2_base->set_seed_language(language);
     cout << "Seed language: " << w2_base->get_seed_language() << endl;
     crypto::secret_key secret_key;
