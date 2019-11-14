f65c43652b567ae25a089885a2dbdc576c221fbf
woodser <woodser@protonmail.com>
2019-11-14 17:47:38 -0500

    export wasm wallet and cpp utils

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict';
 
-module.exports = { MoneroWalletRpc: require("./src/main/js/wallet/MoneroWalletRpc"), MoneroWalletLocal: require("./src/main/js/wallet/MoneroWalletLocal"), MoneroDaemonRpc: require("./src/main/js/daemon/MoneroDaemonRpc") };
+module.exports = {
+    MoneroDaemonRpc: require("./src/main/js/daemon/MoneroDaemonRpc"),
+    MoneroWalletRpc: require("./src/main/js/wallet/MoneroWalletRpc"),
+    MoneroWalletWasmPromise: require("./src/main/js/wallet/MoneroWalletWasm"),
+    MoneroWalletLocal: require("./src/main/js/wallet/MoneroWalletLocal"),
+    MoneroCppUtilsPromise: require("./src/main/js/utils/MoneroCppUtils")
+};
