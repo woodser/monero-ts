217e590127cf7e715d66615e4db2a20a6231621d
woodser <woodser@protonmail.com>
2019-12-24 22:16:24 -0500

    openssl building by passing no-tests

--- bin/build-openssl-emscripten.sh
+++ bin/build-openssl-emscripten.sh
@@ -2,7 +2,7 @@
 
 PLATFORM="emscripten"
 
-SRC_DIR="external/monero-cpp-library/external/openssl"
+SRC_DIR="external/monero-cpp-library/external/openssl-sdk"
 INSTALL_DIR="build/openssl"
 
 SRC_PATH="$(pwd)/$SRC_DIR"
@@ -18,10 +18,10 @@ fi
 cd "$SRC_PATH"
 
 #linux-generic32 \
-#darwin64-x86_64-cc \
 
-emconfigure ./config \
-	-no-asm no-ssl2 no-ssl3 no-comp no-engine no-deprecated shared no-dso \
+emconfigure ./Configure \
+	linux-generic32 \
+	-no-asm no-ssl2 no-ssl3 no-comp no-engine no-deprecated no-tests shared no-dso \
 	--prefix="$INSTALL_PATH" \
 	--openssldir="$INSTALL_PATH" \
 	2>&1
@@ -31,16 +31,12 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-# now must manually tweak the Makefile as I'm not sure how to do this via ./Configure
+# set CROSS_COMPILE to blank
 to_cross_compile_line='CROSS_COMPILE='
 sed -i.bak 's/^CROSS_COMPILE=\/.*$/'"$to_cross_compile_line"'/' Makefile #must match whole line with start char or we might do it repeatedly .. though this particular one would be idempotent anyway
 # ^-- not 'g' b/c we only expect one
 
-# manually remove 'CNF_CFLAGS=-arch x86_64' property
-to_cnf_cflags_line='CNF_CFLAGS='
-sed -i.bak 's/^CNF_CFLAGS=-arch x86_64/'"$to_cnf_cflags_line"'/' Makefile #must match whole line with start char or we might do it repeatedly .. though this particular one would be idempotent anyway
-# ^-- not 'g' b/c we only expect one
-
+# set define_atomics
 to_defined_atomics_line='\&\& !defined(__STDC_NO_ATOMICS__) \&\& !defined(__EMSCRIPTEN__)'
 sed -i.bak 's/\&\&\ !defined(__STDC_NO_ATOMICS__)$/'"$to_defined_atomics_line"'/' include/internal/refcount.h #the pattern is relying here on the fact the "ATOMICS__)" comes at the end of the line
 # ^-- not 'g' b/c we only expect one
