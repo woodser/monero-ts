4fb23d38f27e2c736112073e1e3bdc350b2afde6
woodser <woodser@protonmail.com>
2019-12-27 11:26:20 -0500

    build boost regex
    Former-commit-id: 6c084dc25067b80468b876f4cbdfa739eed0e8c0

--- bin/build-boost-emscripten.sh
+++ bin/build-boost-emscripten.sh
@@ -38,7 +38,7 @@ export NO_BZIP2=1 #bc it's supplied by emscripten but b2 will fail to find it
 
 
 ./bootstrap.sh \
-  --with-libraries=system,thread,chrono,serialization \
+  --with-libraries=system,thread,chrono,serialization,regex \
 2>&1
 
 if [ $? != 0 ]; then
