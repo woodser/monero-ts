7a658ec65db65554173d0675a01f1d9f05f8b3a5
woodser <woodser@protonmail.com>
2020-01-05 09:36:18 -0500

    build script uses libs built in monero-core submodule

--- bin/build-libmonero-cpp.sh
+++ bin/build-libmonero-cpp.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Make libmonero-cpp.dylib
+cd ./external/monero-core/ && 
+make release-static -j8 &&
+
+# Make libmonero-cpp.dylib
+cd ../../ &&
 mkdir -p build && 
 cd build && 
 cmake .. && 
