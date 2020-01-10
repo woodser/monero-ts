0ffac583a2ee3e4a9466125f22a73fb8abdceacf
woodser <woodser@protonmail.com>
2020-01-10 09:07:06 -0500

    fix http client

--- src/test/TestAll.js
+++ src/test/TestAll.js
@@ -8,8 +8,8 @@ const TestMoneroWalletRpc = require("./TestMoneroWalletRpc");
 const TestMoneroWalletKeys = require("./TestMoneroWalletKeys");
 const TestMoneroWalletCore = require("./TestMoneroWalletCore");
 
-// test sample code for readme
-require("./TestSampleCode");
+//// test sample code for readme
+//require("./TestSampleCode");
 
 // test utilitiles
 require("./TestMoneroUtils");
@@ -31,23 +31,23 @@ require("./TestMoneroUtils");
 //  testResets: false
 //});
 
-//// test keys-only wallet
-//new TestMoneroWalletKeys().runTests({
-//  liteMode: false,
-//  testNonRelays: true,
-//  testRelays: false,
-//  testResets: false,
-//  testNotifications: false
-//});
+// test keys-only wallet
+new TestMoneroWalletKeys().runTests({
+  liteMode: false,
+  testNonRelays: true,
+  testRelays: false,
+  testResets: false,
+  testNotifications: false
+});
 
-//// test core wallet
-//new TestMoneroWalletCore().runTests({
-//  liteMode: false,
-//  testNonRelays: true,
-//  testRelays: false,
-//  testResets: false,
-//  testNotifications: false
-//});
+// test core wallet
+new TestMoneroWalletCore().runTests({
+  liteMode: false,
+  testNonRelays: true,
+  testRelays: false,
+  testResets: false,
+  testNotifications: false
+});
 
 //// test boolean set (data structure used by wallet to track progress)
 //require("./TestBooleanSet");
