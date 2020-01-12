f89dab3dd3b027ba2ea0a5e5e20e0e0f0c2c8be2
woodser <woodser@protonmail.com>
2020-01-12 10:15:40 -0500

    MoneroRpcConnection supports SSL; accept self-signed certificates with rejectUnauthorized: false

--- src/test/utils/TestUtils.js
+++ src/test/utils/TestUtils.js
@@ -165,7 +165,8 @@ TestUtils.WALLET_RPC_CONFIG = {
   uri: "http://localhost:38083",
   user: "rpc_user",
   pass: "abc123",
-  maxRequestsPerSecond: 500
+  maxRequestsPerSecond: 500,
+  rejectUnauthorized: false // accept self-signed certificates
 };
 
 // daemon RPC config
@@ -173,7 +174,8 @@ TestUtils.DAEMON_RPC_CONFIG = {
   uri: "http://localhost:38081",
   user: "superuser",
   pass: "abctesting123",
-  maxRequestsPerSecond: 500
+  maxRequestsPerSecond: 500,
+  rejectUnauthorized: false // accept self-signed certificates
 };
 
 // used to track which wallets are in sync with pool so associated txs in the pool do not need to be waited on
