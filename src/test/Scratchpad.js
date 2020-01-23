71d03a3a6f07794f8fbc97589e35a7230f77069d
woodser <woodser@protonmail.com>
2020-01-23 06:59:34 -0500

    update scratchpad
    Former-commit-id: a247a8af76f0cd26046de6ab246595ca1667918c

--- src/test/Scratchpad.js
+++ src/test/Scratchpad.js
@@ -4,7 +4,8 @@ describe("Scratchpad", function() {
     
     // initialize daemon, wallet, and direct rpc interface
     let daemon = TestUtils.getDaemonRpc();
-    let wallet = await TestUtils.getWalletRpc();
+    let walletRpc = await TestUtils.getWalletRpc();
+    let walletCore = await TestUtils.getWalletCore();
     let rpc = new MoneroRpcConnection(TestUtils.WALLET_RPC_CONFIG);
     
 //  try { await wallet.startMining(8, false, true); }
@@ -15,14 +16,14 @@ describe("Scratchpad", function() {
 //  await wallet.rescanSpent();
 //  await daemon.flushTxPool();
     
-    // common variables
-    let txs;
-    let transfers;
-    let txHash;
+    // send a transaction to the test wallet
+    console.log("Daemon height: " + await daemon.getHeight());
+    let txSet = await walletCore.send(0, "54tjXUgQVYNXQCJM4CatRQZMacZ2Awq4NboKiUYtUJrhgYZjiDhMz4ccuYRcMTno6V9mzKFXzfY8pbPnGmu2ukfWABV75k4", (await walletCore.getUnlockedBalance(0)).divide(2));
+    console.log(txSet);
     
     // -------------------------------- SCRATCHPAD ----------------------------
     
-    let outputs = await wallet.getOutputs(new MoneroOutputQuery().setAccountIndex(0).setSubaddressIndex(1));
+    let outputs = await walletRpc.getOutputs(new MoneroOutputQuery().setAccountIndex(0).setSubaddressIndex(1));
     assert(outputs.length > 0);
     for (let output of outputs) {
       assert.equal(output.getAccountIndex(), 0);
