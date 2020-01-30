26ba48e3de7773e372fca03611fdab18c6c36319
TrasherDK <trasherdk@fumlersoft.dk>
2020-01-30 09:08:43 +0100

    Strip trailing slash from config.uri

--- src/main/js/rpc/MoneroRpcConnection.js
+++ src/main/js/rpc/MoneroRpcConnection.js
@@ -53,7 +53,8 @@ class MoneroRpcConnection {
     
     // standardize uri
     if (this.config.uri) {
-      // TODO: strip trailing slash
+      this.config.uri = this.config.uri.replace(/\/$/, ""); // One trailing slash
+//      this.config.uri = this.config.uri.replace(/\/+$/, ""); // All trailing slash
     } else {
       this.config.uri = this.config.protocol + "://" + this.config.host + ":" + this.config.port;
     }
