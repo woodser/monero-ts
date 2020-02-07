5c5557b2b4f29dc799bba771416eb40b909c3ad5
woodser <woodser@protonmail.com>
2020-02-07 11:14:42 -0500

    full test config
    Former-commit-id: fbef7df69ec97265866e8918ea84d7befa0a3a36

--- src/test/MoneroTestModel.js
+++ src/test/MoneroTestModel.js
@@ -3,6 +3,7 @@
  */
 module.exports = function() {
   require("./utils/TestUtilsModule")();
+  this.TestSampleCode = require("./TestSampleCode");
   this.TestMoneroUtils = require("./TestMoneroUtils");
   this.TestMoneroDaemonRpc = require("./TestMoneroDaemonRpc");
   this.TestMoneroWalletRpc = require("./TestMoneroWalletRpc");
--- src/test/TestAll.js
+++ src/test/TestAll.js
@@ -11,42 +11,37 @@ const TestMoneroWalletCore = require("./TestMoneroWalletCore");
 // test config
 //TestUtils.FS = require('memfs');    // use in-memory file system needed for tests since running in the browser
 
-//// test sample code for readme
-//new TestSampleCode().runTests();
+// test sample code for readme
+new TestSampleCode().runTests();
 
-//// test utilitiles
-//new TestMoneroUtils().runTests();
+// test utilitiles
+new TestMoneroUtils().runTests();
 
-//// test daemon rpc
-//new TestMoneroDaemonRpc({
-//  liteMode: true,  // skips some thorough but lengthy tests
-//  testNonRelays: true,
-//  testRelays: false, // creates and relays outgoing txs
-//  testNotifications: false
-//}).runTests();
+// test daemon rpc
+new TestMoneroDaemonRpc({
+  liteMode: false,  // skips some thorough but lengthy tests
+  testNonRelays: true,
+  testRelays: true, // creates and relays outgoing txs
+  testNotifications: true
+}).runTests();
 
-//// test wallet rpc
-//new TestMoneroWalletRpc({
-//  liteMode: true, // skips some lengthy but detailed tests
-//  testNonRelays: true,
-//  testRelays: true,
-//  testNotifications: false,
-//  testResets: false
-//}).runTests();
+// test wallet rpc
+new TestMoneroWalletRpc({
+  liteMode: false, // skips some lengthy but detailed tests
+  testNonRelays: true,
+  testRelays: true,
+  testNotifications: false,
+  testResets: false
+}).runTests();
 
-//// test keys-only wallet
-//new TestMoneroWalletKeys({
-//  liteMode: false,
-//  testNonRelays: true,
-//  testRelays: false,
-//  testResets: false,
-//  testNotifications: false
-//}).runTests();
-
-//const memfs = require('memfs');
-//const Volume = memfs.Volume;
-//let vol = new Volume();
-//let fs = memfs.createFsFromVolume(vol);
+// test keys-only wallet
+new TestMoneroWalletKeys({
+  liteMode: false,
+  testNonRelays: true,
+  testRelays: false,
+  testResets: false,
+  testNotifications: false
+}).runTests();
 
 // test core wallet
 new TestMoneroWalletCore({
@@ -54,6 +49,7 @@ new TestMoneroWalletCore({
   testNonRelays: true,
   testRelays: false,
   testResets: false,
+  testNotifications: true
 }).runTests();
 
 // test scratchpad
