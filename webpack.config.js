49e2a5e3d553760305d35b59a6c9d9ffefa05350
woodser <woodser@protonmail.com>
2020-02-21 12:30:20 -0500

    build MoneroWebWorker.js to ./dist
    - add build-browser.sh
    - add monero-javascript as dev dependency of self to build worker
    
    Former-commit-id: 88589399b29f320deda056c27efffedff1acc417

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+let configBase = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: path.join(__dirname, 'node_modules'),
+          type: "javascript/auto",
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: false
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devtool: 'source-map',
+    externals: ['worker_threads','ws','perf_hooks'], // exclude nodejs
+    resolve: {
+      alias: {
+        "fs": "html5-fs"
+      },
+      extensions: ['.js', '.jsx', '.css', '.json', 'otf', 'ttf', 'eot', 'svg'],
+      modules: [
+        'node_modules'
+      ]
+    },
+    cache: true,
+    context: __dirname
+};
+
+let configMoneroWebWorker = Object.assign({}, configBase, {
+    name: "Monero web worker config",
+    entry: "./src/main/js/utils/MoneroWebWorker.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "MoneroWebWorker.js"
+    },
+});
+
+module.exports = [configMoneroWebWorker];
