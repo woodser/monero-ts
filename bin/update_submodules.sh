6e9a058834ca7c0de5086fd86e4fd93ccb5d2f10
TrasherDK <trasherdk@fumlersoft.dk>
2020-03-04 07:34:56 +0100

    Ensure external exist and submodule is active

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+[ -d external ] \
+|| {
+  mkdir external
+  git submodule add -f https://github.com/woodser/monero-cpp-library.git external/monero-cpp-library
+}
+
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
 git submodule foreach --recursive git checkout master
