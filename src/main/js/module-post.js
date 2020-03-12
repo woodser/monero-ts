26d756102526ff2824c804aff00dc3ae54175e5c
woodser <woodser@protonmail.com>
2020-03-12 15:24:09 -0400

    enable wasm exception handling :)
    build with exception whitelist from wasm bridge
    remove module-post.js
    
    Former-commit-id: e29d303e0dcdb6be7e946baa1fe210a5fca54c0a

--- src/main/js/module-post.js
+++
@@ -1,9 +0,0 @@
-Module['ready'] = new Promise(function (resolve, reject) {
-  delete Module['then']
-  Module['onAbort'] = function (what) {
-    reject(what)
-  }
-  addOnPostRun(function () {
-    resolve(Module)
-  })
-})
