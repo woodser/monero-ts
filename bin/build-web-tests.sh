9170b4aa454042933d56abcee1b489f2f0dd9a7b
woodser <woodser@protonmail.com>
2020-03-27 22:28:57 -0400

    cleanup

--- bin/build-web-tests.sh
+++ bin/build-web-tests.sh
@@ -4,7 +4,7 @@
 #./bin/build-wasm-emscripten.sh
 npm run build-web-tests
 
-# copy app dependencies
+# copy web app dependencies
 cp -R dist/ browser_build/
 cp src/test/browser/tests.html browser_build/tests.html
 cp node_modules/mocha/mocha.js browser_build/mocha.js
