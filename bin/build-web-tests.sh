ea158ba6267148efd6641880811ba1091aa3cdfb
woodser <woodser@protonmail.com>
2020-03-27 22:20:35 -0400

    add browser tests build

---
+++ bin/build-web-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# build web app dependencies
+#./bin/build-wasm-emscripten.sh
+npm run build-web-tests
+
+# copy app dependencies
+cp -R dist/ browser_build/
+cp src/test/browser/tests.html browser_build/tests.html
+cp node_modules/mocha/mocha.js browser_build/mocha.js
+cp node_modules/mocha/mocha.css browser_build/mocha.css
+
+# start server
+cd browser_build && python ../bin/run_server.py
---
+++ src/test/browser/tests.js
@@ -0,0 +1,83 @@
+/**
+ * Sample browser application which uses a JavaScript library to interact
+ * with a Monero daemon using RPC and a Monero wallet using RPC and WASM
+ * bindings.
+ */
+
+// import library models
+require("../../../index.js");
+
+// import test types
+require("../utils/TestUtilsModule")();
+const TestMoneroDaemonRpc = require("../TestMoneroDaemonRpc");
+const TestMoneroWalletRpc = require("../TestMoneroWalletRpc");
+const TestMoneroWalletKeys = require("../TestMoneroWalletKeys");
+const TestMoneroWalletCore = require("../TestMoneroWalletCore");
+
+/**
+ * Run tests when document ready.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+  runTests();
+});
+
+/**
+ * Run Monero tests.
+ */
+function runTests() {
+  
+  // mocha setup
+  mocha.setup({
+    ui: 'bdd',
+    timeout: 3000000
+  });
+  mocha.checkLeaks();
+  
+  // test config
+  TestUtils.PROXY_TO_WORKER = true; // proxy test wallet and daemon to worker to not lock main browser thread
+  TestUtils.FS = require('memfs');  // use in-memory file system needed for tests since running in the browser
+  
+  // test sample code for readme
+  new TestSampleCode().runTests();
+  
+  // test utilitiles
+  new TestMoneroUtils().runTests();
+  
+  // test daemon rpc
+  new TestMoneroDaemonRpc({
+    liteMode: true,          // skips some thorough but lengthy tests
+    testNonRelays: true,
+    testRelays: true,         // creates and relays outgoing txs
+    testNotifications: true,
+  }).runTests();
+  
+  // test wallet rpc
+  new TestMoneroWalletRpc({
+    liteMode: false, // skips some lengthy but detailed tests
+    testNonRelays: true,
+    testRelays: true,
+    testNotifications: false,
+    testResets: false
+  }).runTests();
+  
+  // test keys-only wallet
+  new TestMoneroWalletKeys({
+    liteMode: false,
+    testNonRelays: true,
+    testRelays: false,
+    testResets: false,
+    testNotifications: false
+  }).runTests();
+  
+  // test core wallet
+  new TestMoneroWalletCore({
+    liteMode: false,
+    testNonRelays: true,
+    testRelays: true,
+    testResets: false,
+    testNotifications: true,
+  }).runTests();
+  
+  // run tests
+  mocha.run();
+}
