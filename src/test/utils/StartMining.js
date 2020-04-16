a48479577b1ee13e8f7d4d3a13f5d7eec7fbff50
woodser <woodser@protonmail.com>
2020-04-16 16:38:24 -0400

    combine js/utils and js/rpc into js/common
    Former-commit-id: 6cfcdc20a6933f40ef52f1df0ace0565395b7de4

--- src/test/utils/StartMining.js
+++ src/test/utils/StartMining.js
@@ -5,7 +5,7 @@ class StartMining {
   
   static async startMining() {
     let numThreads = 6;
-    //utils/TestUtils.getWalletRpc().startMining(numThreads, false, true);
+    //TestUtils.getWalletRpc().startMining(numThreads, false, true);
     let daemon = await TestUtils.getDaemonRpc();
     await daemon.startMining("56SWsnhejUTbgNs2EgyXdfNXUawymMMuAC9voZZSQrHzJHNxGsAvMnoUja7JcKVtPwNc1oKAkoAt1cv6EmtKRQ22U37B7cT", numThreads, false, false);  // random subaddress
   }
--- webpack.worker.js
+++ webpack.worker.js
@@ -5,7 +5,7 @@ const configBase = require("./webpack.base.js");
 
 let configMoneroWebWorker = Object.assign({}, configBase, {
   name: "Monero web worker config",
-  entry: "./src/main/js/utils/MoneroWebWorker.js",
+  entry: "./src/main/js/common/MoneroWebWorker.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "MoneroWebWorker.dist.js"
