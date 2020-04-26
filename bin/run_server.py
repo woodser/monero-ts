70a6da79633b38f0a26712124da9f3f74bda4655
woodser <woodser@protonmail.com>
2020-04-26 07:42:59 -0400

    update to python 3

--- bin/run_server.py
+++ bin/run_server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import BaseHTTPServer, SimpleHTTPServer
+import http.server
 #import ssl
  
 port=9100
 print("Running on port %d" % port)
  
-SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] =    'application/wasm' 
-httpd = BaseHTTPServer.HTTPServer(('localhost', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+http.server.SimpleHTTPRequestHandler.extensions_map['.wasm'] =    'application/wasm' 
+httpd = http.server.HTTPServer(('localhost', port), http.server.SimpleHTTPRequestHandler)
 #httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="../src/test/browser/localhost-key.pem", certfile='../src/test/browser/localhost-cert.pem', server_side=True)
  
 httpd.serve_forever()
