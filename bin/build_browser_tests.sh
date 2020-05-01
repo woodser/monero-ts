a9ca879d6906159748262e9cb6b7c52732022357
woodser <woodser@protonmail.com>
2020-05-01 17:43:52 -0400

    add favicon for browser tests

--- bin/build_browser_tests.sh
+++ bin/build_browser_tests.sh
@@ -10,6 +10,7 @@ npm run build_web_tests || exit 1
 # copy dependencies to browser build
 cp -R dist/ browser_build/ || exit 1
 cp src/test/browser/tests.html browser_build/tests.html || exit 1
+cp src/test/browser/favicon.ico browser_build/favicon.ico || exit 1
 cp node_modules/mocha/mocha.js browser_build/mocha.js || exit 1
 cp node_modules/mocha/mocha.css browser_build/mocha.css || exit 1
 
