a75122d4194cd31662f9d7660f197b73d5e270c8
woodser <woodser@protonmail.com>
2020-05-02 10:03:08 -0400

    cleanup

--- bin/run_server.py
+++ bin/run_server.py
@@ -5,7 +5,7 @@ import http.server
 port=9100
 print("Running on port %d" % port)
  
-http.server.SimpleHTTPRequestHandler.extensions_map['.wasm'] =    'application/wasm' 
+http.server.SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm' 
 httpd = http.server.HTTPServer(('localhost', port), http.server.SimpleHTTPRequestHandler)
 #httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="../src/test/browser/localhost-key.pem", certfile='../src/test/browser/localhost-cert.pem', server_side=True)
  
