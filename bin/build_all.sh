15ec4d483034701bef9c7883570908297ec525a2
woodser <woodser@protonmail.com>
2020-06-02 19:43:19 -0400

    exit 1 on errors

--- bin/build_all.sh
+++ bin/build_all.sh
@@ -12,7 +12,7 @@
   }
 
 # update submodules
-./bin/update_submodules.sh || exit
+./bin/update_submodules.sh || exit 1
 
 # build monero-core translations directory
 cd ./external/monero-cpp-library/external/monero-core || exit 1
@@ -23,19 +23,19 @@ make release-static -j8		# don't exit because this will build translations direc
 cd ../../../../ || exit 1
 
 # checkout monero-cpp-library master branch
-git --git-dir ./external/monero-cpp-library/.git checkout master || exit
+git --git-dir ./external/monero-cpp-library/.git checkout master || exit 1
 
 # build boost
-./bin/build_boost_emscripten.sh || exit
+./bin/build_boost_emscripten.sh || exit 1
 
 # build openssl
-./bin/build_openssl_emscripten.sh || exit
+./bin/build_openssl_emscripten.sh || exit 1
 
 # build webassembly for distribution
-./bin/build_wasm_emscripten.sh || exit
+./bin/build_wasm_emscripten.sh || exit 1
 
 # build web worker
-./bin/build_web_worker.sh || exit
+./bin/build_web_worker.sh || exit 1
 
 # build browser tests
-./bin/build_browser_tests.sh || exit
+./bin/build_browser_tests.sh || exit 1
