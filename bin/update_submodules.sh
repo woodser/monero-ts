330b6079b0d2776f0ae503eaef8c65a0d4b65b41
woodser <woodser@protonmail.com>
2020-06-03 14:39:27 -0400

    pull current branch in ./bin/update_submodules.sh

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -2,4 +2,4 @@
 
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
-git submodule foreach --recursive git pull
+git submodule foreach --recurive git pull origin $(git rev-parse --abbrev-ref HEAD)
