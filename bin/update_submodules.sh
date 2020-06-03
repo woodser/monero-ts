30b25a219d84be0cc0e55c613cb60c408a3e20ff
woodser <woodser@protonmail.com>
2020-06-03 14:30:28 -0400

    don't checkout master in ./bin/update_submodules.sh

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -2,5 +2,4 @@
 
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
-git submodule foreach --recursive git checkout master
-git submodule foreach --recursive git pull --ff-only origin master
+git submodule foreach --recursive git pull
