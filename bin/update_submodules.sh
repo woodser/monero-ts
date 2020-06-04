d94815510ff2167a1cb0042ec47e1d3a52f34c19
woodser <woodser@protonmail.com>
2020-06-04 14:17:45 -0400

    only update submodules

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -3,10 +3,6 @@
 # initialize submodules recursively
 git submodule update --init --recursive
 
-# update monero-javascript
-git checkout master
-git pull --ff-only origin master
-
 # update monero-cpp
 cd ./external/monero-cpp
 git checkout master
