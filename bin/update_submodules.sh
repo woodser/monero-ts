752c48627e7a7ee73cf54e62c36b4a0261e05316
woodser <woodser@protonmail.com>
2020-06-04 13:42:22 -0400

    recursively initialize submodules once then checkout specific version

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
+# initialize submodules recursively
+git submodule update --init --recursive
+
 # update monero-javascript
-git submodule update --init
 git checkout master
 git pull --ff-only origin master
 
 # update monero-cpp
 cd ./external/monero-cpp
-git submodule update --init
 git checkout master
 git pull --ff-only origin master
 
 # update monero-core
 cd ./external/monero-core
-git submodule update --init
 git checkout master
 git pull --ff-only origin master
 cd ../../../../
