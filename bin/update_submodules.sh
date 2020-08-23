99e32ce54f5b79031ffb37f92a0bbf1b34b8366a
woodser <woodser@protonmail.com>
2020-08-23 08:28:51 -0400

    bump version to 0.4.3, update monero-cpp submodule to v0.3.5

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -5,8 +5,8 @@ git submodule update --init --recursive
 
 # update monero-cpp
 cd ./external/monero-cpp
-git checkout tags/v0.3.4
-git pull --ff-only origin tags/v0.3.4
+git checkout tags/v0.3.5
+git pull --ff-only origin tags/v0.3.5
 
 # update monero-core
 cd ./external/monero-core
