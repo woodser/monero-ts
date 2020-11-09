5174c108f7f8beead414a4f4bd7356a04f8c0bdf
woodser <woodser@protonmail.com>
2020-11-09 11:33:34 -0500

    copy boost *.a from wildcard path instead of hardcoding compiler

--- bin/build_libmonero_cpp.sh
+++ bin/build_libmonero_cpp.sh
@@ -9,15 +9,15 @@ cd ../../
 # copy boost .a files to ./external-libs/boost
 mkdir -p ./external-libs || exit 1
 mkdir -p ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/chrono/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_chrono.a ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/date_time/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_date_time.a ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/filesystem/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_filesystem.a ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/program_options/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_program_options.a ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/regex/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_regex.a  ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/serialization/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_serialization.a ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/serialization/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_wserialization.a ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/system/build/clang-darwin-11.0/release/link-static/threading-multi/visibility-hidden/libboost_system.a ./external-libs/boost || exit 1
-cp ./external/boost-sdk/bin.v2/libs/thread/build/clang-darwin-11.0/release/link-static/threadapi-pthread/threading-multi/visibility-hidden/libboost_thread.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/chrono/build/*/release/link-static/threading-multi/visibility-hidden/libboost_chrono.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/date_time/build/*/release/link-static/threading-multi/visibility-hidden/libboost_date_time.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/filesystem/build/*/release/link-static/threading-multi/visibility-hidden/libboost_filesystem.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/program_options/build/*/release/link-static/threading-multi/visibility-hidden/libboost_program_options.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/regex/build/*/release/link-static/threading-multi/visibility-hidden/libboost_regex.a  ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/serialization/build/*/release/link-static/threading-multi/visibility-hidden/libboost_serialization.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/serialization/build/*/release/link-static/threading-multi/visibility-hidden/libboost_wserialization.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/system/build/*/release/link-static/threading-multi/visibility-hidden/libboost_system.a ./external-libs/boost || exit 1
+cp ./external/boost-sdk/bin.v2/libs/thread/build/*/release/link-static/threadapi-pthread/threading-multi/visibility-hidden/libboost_thread.a ./external-libs/boost || exit 1
 
 # copy openssl .a files to ./external-libs/openssl
 mkdir -p ./external-libs/openssl || exit 1
