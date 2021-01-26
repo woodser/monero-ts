70a0e3318429652bb730cd3d026d0ef315e880f7
woodser <woodser@protonmail.com>
2021-01-26 12:05:51 -0500

    checkout monero-cpp tags/v0.5.1

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -5,8 +5,8 @@ git submodule update --init --recursive
 
 # update monero-cpp
 cd ./external/monero-cpp
-git checkout master
-git pull --ff-only origin master
+git checkout tags/v0.5.1
+git pull --ff-only origin tags/v0.5.1
 
 # update monero-project
 cd ./external/monero-project
