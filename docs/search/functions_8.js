71e2b437b7d66c5ae264ce74e61c55008cb544af
woodser <woodser@protonmail.com>
2021-03-09 10:03:32 -0500

    update doxygen docs

--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['parse_5fpayment_5furi_299',['parse_payment_uri',['../classmonero_1_1monero__wallet.html#afc683a0719a7c8fe4aa80696e9d3eb5b',1,'monero::monero_wallet::parse_payment_uri()'],['../classmonero_1_1monero__wallet__full.html#a4e92f7edbc4e00ec7e219a6ccbee6242',1,'monero::monero_wallet_full::parse_payment_uri()']]],
-  ['parse_5ftx_5fset_300',['parse_tx_set',['../classmonero_1_1monero__wallet.html#a40e9ad5cf8ea958de12f20c5ed396268',1,'monero::monero_wallet::parse_tx_set()'],['../classmonero_1_1monero__wallet__full.html#af74d7629425aea89327c86589f2263c8',1,'monero::monero_wallet_full::parse_tx_set()']]],
+  ['parse_5fpayment_5furi_300',['parse_payment_uri',['../classmonero_1_1monero__wallet.html#afc683a0719a7c8fe4aa80696e9d3eb5b',1,'monero::monero_wallet::parse_payment_uri()'],['../classmonero_1_1monero__wallet__full.html#a4e92f7edbc4e00ec7e219a6ccbee6242',1,'monero::monero_wallet_full::parse_payment_uri()']]],
   ['prepare_5fmultisig_301',['prepare_multisig',['../classmonero_1_1monero__wallet.html#a8f9b31c8abcbb90caf1c4c00538dbf83',1,'monero::monero_wallet::prepare_multisig()'],['../classmonero_1_1monero__wallet__full.html#a0e2e12d8e7613569197029a4b0785478',1,'monero::monero_wallet_full::prepare_multisig()']]]
 ];
