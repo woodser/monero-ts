07b6e5bdcd5982eeb9a1e0f86099240b42ab9529
woodser <woodser@protonmail.com>
2021-03-10 15:56:37 -0500

    checkout monero-cpp v0.5.2 and monero-project with pr 7321

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -5,11 +5,11 @@ git submodule update --init --recursive
 
 # update monero-cpp
 cd ./external/monero-cpp
-git checkout master
-git pull --ff-only origin master
+git checkout tags/v0.5.2
+git pull --ff-only origin tags/v0.5.2
 
 # update monero-project
 cd ./external/monero-project
-git checkout master
-git pull --ff-only origin master
+git checkout tags/pr_7321
+git pull --ff-only origin tags/pr_7321
 cd ../../../../
