ea186de708d9d5e8ee5ac2fff6c131aca9e75729
woodser <woodser@protonmail.com>
2021-08-12 17:27:57 -0400

    update to webpack 5.50.0, manually add polyfills

--- webpack.base.js
+++ webpack.base.js
@@ -1,8 +1,10 @@
 "use strict"
 
 const path = require("path");
+const webpack = require('webpack')
 
 let configBase = {
+    mode: 'production',
     module: {
       rules: [
         {
@@ -22,6 +24,12 @@ let configBase = {
     },
     devtool: 'source-map',
     externals: ['worker_threads','ws','perf_hooks', 'child_process'], // exclude nodejs
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      }),
+    ],
     resolve: {
       alias: {
         "fs": "html5-fs"
@@ -29,7 +37,32 @@ let configBase = {
       extensions: ['.js', '.jsx', '.css', '.json', 'otf', 'ttf', 'eot', 'svg'],
       modules: [
         'node_modules'
-      ]
+      ],
+      fallback: { // browser polyfills
+        assert: require.resolve('assert'),
+        //buffer: require.resolve('buffer'),
+        //console: require.resolve('console-browserify'),
+        //constants: require.resolve('constants-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        //domain: require.resolve('domain-browser'),
+        //events: require.resolve('events'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        path: require.resolve('path-browserify'),
+        //punycode: require.resolve('punycode'),
+        //process: require.resolve('process/browser'),
+        querystring: require.resolve('querystring-es3'),
+        stream: require.resolve('stream-browserify'),
+        //string_decoder: require.resolve('string_decoder'),
+        //sys: require.resolve('util'),
+        //timers: require.resolve('timers-browserify'),
+        //tty: require.resolve('tty-browserify'),
+        url: require.resolve('url'),
+        util: require.resolve('util'),
+        //vm: require.resolve('vm-browserify'),
+        zlib: require.resolve('browserify-zlib')
+      }
     },
     cache: true,
     context: __dirname
