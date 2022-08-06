22593068f643fab2978f618d803134abac644eb8
woodser <woodser@protonmail.com>
2022-08-06 12:57:01 -0400

    update api docs

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['freeze_5foutput_28',['freeze_output',['../classmonero_1_1monero__wallet.html#a3e19e09a0139c773bd51a692a15cb750',1,'monero::monero_wallet::freeze_output()'],['../classmonero_1_1monero__wallet__full.html#a292a177bc1d7fe3f583abe8e7bb7bad1',1,'monero::monero_wallet_full::freeze_output()']]]
+  ['freeze_5foutput_27',['freeze_output',['../classmonero_1_1monero__wallet.html#a3e19e09a0139c773bd51a692a15cb750',1,'monero::monero_wallet::freeze_output()'],['../classmonero_1_1monero__wallet__full.html#a292a177bc1d7fe3f583abe8e7bb7bad1',1,'monero::monero_wallet_full::freeze_output()']]]
 ];
--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['make_5fmultisig_299',['make_multisig',['../classmonero_1_1monero__wallet.html#a98d17406e8da363e0bab6c155aa87267',1,'monero::monero_wallet::make_multisig()'],['../classmonero_1_1monero__wallet__full.html#ab3bd9f79ad125bd5060dabf61faa2d98',1,'monero::monero_wallet_full::make_multisig()']]],
+  ['make_5fmultisig_299',['make_multisig',['../classmonero_1_1monero__wallet.html#a04d5bd95d2c595a103186cb116c6bbd3',1,'monero::monero_wallet::make_multisig()'],['../classmonero_1_1monero__wallet__full.html#ac28b11afbadc722da14b46302a061c03',1,'monero::monero_wallet_full::make_multisig()']]],
   ['merge_5ftx_300',['merge_tx',['../namespacemonero.html#a33c4fb9da9e3f83543bb4bc0da940fd9',1,'monero']]],
   ['move_5fto_301',['move_to',['../classmonero_1_1monero__wallet.html#a85c3484ccfea43aa624c3c8f1bd3ab8b',1,'monero::monero_wallet::move_to()'],['../classmonero_1_1monero__wallet__full.html#abd885f1632f345e085652803e5f9d6d4',1,'monero::monero_wallet_full::move_to()']]]
 ];
