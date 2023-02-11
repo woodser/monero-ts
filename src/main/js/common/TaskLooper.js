356c4197823657b88d666fdbcb3b6119f3335e51
woodser <woodser@protonmail.com>
2023-02-11 13:15:14 -0500

    TaskLooper supports getTask(), isStarted(), and chaining

--- src/main/js/common/TaskLooper.js
+++ src/main/js/common/TaskLooper.js
@@ -6,31 +6,51 @@ class TaskLooper {
   /**
    * Build the looper with a function to invoke on a fixed period loop.
    * 
-   * @param {function} fn - the function to invoke
+   * @param {function} task - the task function to invoke
    */
-  constructor(fn) {
-    this.fn = fn;
+  constructor(task) {
+    this._task = task;
+  }
+
+  /**
+   * Get the task function to invoke on a fixed period loop.
+   * 
+   * @return {function} the task function
+   */
+  getTask() {
+    return this._task;
   }
   
   /**
    * Start the task loop.
    * 
    * @param {int} periodInMs the loop period in milliseconds
+   * @return {TaskLooper} this class for chaining
    */
   start(periodInMs) {
-    this.periodInMs = periodInMs;
-    if (this.isStarted) return;
-    this.isStarted = true;
+    this._periodInMs = periodInMs;
+    if (this._isStarted) return this;
+    this._isStarted = true;
     
     // start looping
     this._runLoop();
+    return this;
+  }
+
+  /**
+   * Indicates if looping.
+   * 
+   * @return {boolean} true if looping, false otherwise
+   */
+  isStarted() {
+    return this._isStarted;
   }
   
   /**
    * Stop the task loop.
    */
   stop() {
-    this.isStarted = false;
+    this._isStarted = false;
   }
   
   /**
@@ -39,19 +59,19 @@ class TaskLooper {
    * @param {int} periodInMs the loop period in milliseconds
    */
   setPeriodInMs(periodInMs) {
-    this.periodInMs = periodInMs;
+    this._periodInMs = periodInMs;
   }
   
   async _runLoop() {
-    if (this.isLooping) return;
-    this.isLooping = true;
+    if (this._isLooping) return;
+    this._isLooping = true;
     let that = this;
-    while (this.isStarted) {
+    while (this._isStarted) {
       let startTime = Date.now();
-      await this.fn();
-      if (this.isStarted) await new Promise(function(resolve) { setTimeout(resolve, that.periodInMs - (Date.now() - startTime)); });
+      await this._task();
+      if (this._isStarted) await new Promise(function(resolve) { setTimeout(resolve, that._periodInMs - (Date.now() - startTime)); });
     }
-    this.isLooping = false;
+    this._isLooping = false;
   }
 }
 
