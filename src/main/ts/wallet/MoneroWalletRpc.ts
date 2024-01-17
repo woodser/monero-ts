319fd6ceceaa099a785a4370cb85fd64140f3146
woodser <woodser@protonmail.com>
2024-01-17 07:16:42 -0500

    fix stacktrace on error starting daemon or wallet process

--- src/main/ts/wallet/MoneroWalletRpc.ts
+++ src/main/ts/wallet/MoneroWalletRpc.ts
@@ -1492,70 +1492,74 @@ export default class MoneroWalletRpc extends MoneroWallet {
     let uri;
     let that = this;
     let output = "";
-    return new Promise(function(resolve, reject) {
+    try {
+      return await new Promise(function(resolve, reject) {
       
-      // handle stdout
-      process.stdout.on('data', async function(data) {
-        let line = data.toString();
-        LibraryUtils.log(2, line);
-        output += line + '\n'; // capture output in case of error
+        // handle stdout
+        process.stdout.on('data', async function(data) {
+          let line = data.toString();
+          LibraryUtils.log(2, line);
+          output += line + '\n'; // capture output in case of error
+          
+          // extract uri from e.g. "I Binding on 127.0.0.1 (IPv4):38085"
+          let uriLineContains = "Binding on ";
+          let uriLineContainsIdx = line.indexOf(uriLineContains);
+          if (uriLineContainsIdx >= 0) {
+            let host = line.substring(uriLineContainsIdx + uriLineContains.length, line.lastIndexOf(' '));
+            let unformattedLine = line.replace(/\u001b\[.*?m/g, '').trim(); // remove color formatting
+            let port = unformattedLine.substring(unformattedLine.lastIndexOf(':') + 1);
+            let sslIdx = config.cmd.indexOf("--rpc-ssl");
+            let sslEnabled = sslIdx >= 0 ? "enabled" == config.cmd[sslIdx + 1].toLowerCase() : false;
+            uri = (sslEnabled ? "https" : "http") + "://" + host + ":" + port;
+          }
+          
+          // read success message
+          if (line.indexOf("Starting wallet RPC server") >= 0) {
+            
+            // get username and password from params
+            let userPassIdx = config.cmd.indexOf("--rpc-login");
+            let userPass = userPassIdx >= 0 ? config.cmd[userPassIdx + 1] : undefined;
+            let username = userPass === undefined ? undefined : userPass.substring(0, userPass.indexOf(':'));
+            let password = userPass === undefined ? undefined : userPass.substring(userPass.indexOf(':') + 1);
+            
+            // create client connected to internal process
+            config = config.copy().setServer({uri: uri, username: username, password: password, rejectUnauthorized: config.getServer() ? config.getServer().getRejectUnauthorized() : undefined});
+            config.cmd = undefined;
+            let wallet = await MoneroWalletRpc.connectToWalletRpc(config);
+            wallet.process = process;
+            
+            // resolve promise with client connected to internal process 
+            this.isResolved = true;
+            resolve(wallet);
+          }
+        });
         
-        // extract uri from e.g. "I Binding on 127.0.0.1 (IPv4):38085"
-        let uriLineContains = "Binding on ";
-        let uriLineContainsIdx = line.indexOf(uriLineContains);
-        if (uriLineContainsIdx >= 0) {
-          let host = line.substring(uriLineContainsIdx + uriLineContains.length, line.lastIndexOf(' '));
-          let unformattedLine = line.replace(/\u001b\[.*?m/g, '').trim(); // remove color formatting
-          let port = unformattedLine.substring(unformattedLine.lastIndexOf(':') + 1);
-          let sslIdx = config.cmd.indexOf("--rpc-ssl");
-          let sslEnabled = sslIdx >= 0 ? "enabled" == config.cmd[sslIdx + 1].toLowerCase() : false;
-          uri = (sslEnabled ? "https" : "http") + "://" + host + ":" + port;
-        }
+        // handle stderr
+        process.stderr.on('data', function(data) {
+          if (LibraryUtils.getLogLevel() >= 2) console.error(data);
+        });
         
-        // read success message
-        if (line.indexOf("Starting wallet RPC server") >= 0) {
-          
-          // get username and password from params
-          let userPassIdx = config.cmd.indexOf("--rpc-login");
-          let userPass = userPassIdx >= 0 ? config.cmd[userPassIdx + 1] : undefined;
-          let username = userPass === undefined ? undefined : userPass.substring(0, userPass.indexOf(':'));
-          let password = userPass === undefined ? undefined : userPass.substring(userPass.indexOf(':') + 1);
-          
-          // create client connected to internal process
-          config = config.copy().setServer({uri: uri, username: username, password: password, rejectUnauthorized: config.getServer() ? config.getServer().getRejectUnauthorized() : undefined});
-          config.cmd = undefined;
-          let wallet = await MoneroWalletRpc.connectToWalletRpc(config);
-          wallet.process = process;
-          
-          // resolve promise with client connected to internal process 
-          this.isResolved = true;
-          resolve(wallet);
-        }
+        // handle exit
+        process.on("exit", function(code) {
+          if (!this.isResolved) reject(new MoneroError("monero-wallet-rpc process terminated with exit code " + code + (output ? ":\n\n" + output : "")));
+        });
+        
+        // handle error
+        process.on("error", function(err) {
+          if (err.message.indexOf("ENOENT") >= 0) reject(new MoneroError("monero-wallet-rpc does not exist at path '" + config.cmd[0] + "'"));
+          if (!this.isResolved) reject(err);
+        });
+        
+        // handle uncaught exception
+        process.on("uncaughtException", function(err, origin) {
+          console.error("Uncaught exception in monero-wallet-rpc process: " + err.message);
+          console.error(origin);
+          if (!this.isResolved) reject(err);
+        });
       });
-      
-      // handle stderr
-      process.stderr.on('data', function(data) {
-        if (LibraryUtils.getLogLevel() >= 2) console.error(data);
-      });
-      
-      // handle exit
-      process.on("exit", function(code) {
-        if (!this.isResolved) reject(new MoneroError("monero-wallet-rpc process terminated with exit code " + code + (output ? ":\n\n" + output : "")));
-      });
-      
-      // handle error
-      process.on("error", function(err) {
-        if (err.message.indexOf("ENOENT") >= 0) reject(new MoneroError("monero-wallet-rpc does not exist at path '" + config.cmd[0] + "'"));
-        if (!this.isResolved) reject(err);
-      });
-      
-      // handle uncaught exception
-      process.on("uncaughtException", function(err, origin) {
-        console.error("Uncaught exception in monero-wallet-rpc process: " + err.message);
-        console.error(origin);
-        reject(err);
-      });
-    });
+    } catch (err: any) {
+      throw new MoneroError(err.message);
+    }
   }
   
   protected async clear() {
