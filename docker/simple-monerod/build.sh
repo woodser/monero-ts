27ee7f4ffd76ef3eae3c9604fdf46c22e103c185
mainnet-pat <mainnet_pat@protonmail.com>
2024-04-30 11:36:27 +0000

    Add CI test action

--- docker/simple-monerod/build.sh
+++ docker/simple-monerod/build.sh
@@ -3,4 +3,4 @@
 # docker buildx build --build-arg="NPROC=6" --progress plain -t mainnetpat/monerod:v0.18.3.2 --platform linux/arm64/v8 --load .
 # docker buildx build --build-arg="NPROC=6" --progress plain -t mainnetpat/monerod:v0.18.3.2 --platform linux/amd64 --load .
 
-docker buildx build --build-arg="NPROC=6" --progress plain -t mainnetpat/monerod:v0.18.3.2 --platform linux/arm64/v8,linux/amd64 --push .
+docker buildx build --build-arg="NPROC=2" --progress plain -t mainnetpat/monerod:v0.18.3.2 --platform linux/arm64/v8,linux/amd64 --push .
