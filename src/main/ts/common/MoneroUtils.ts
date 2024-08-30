bcb2b3873fa9d7811251d00363798b2caac159f7
woodser <woodser@protonmail.com>
2024-08-30 07:22:43 -0400

    bump version to 0.10.4

--- src/main/ts/common/MoneroUtils.ts
+++ src/main/ts/common/MoneroUtils.ts
@@ -23,7 +23,7 @@ export default class MoneroUtils {
    * @return {string} the version of this monero-ts library
    */
   static getVersion() {
-    return "0.10.3";
+    return "0.10.4";
   }
   
   /**
