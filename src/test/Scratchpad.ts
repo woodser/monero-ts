eff20e89c37af13862585bb5307f415d150d7bee
woodser <13068859+woodser@users.noreply.github.com>
2026-08-26 15:05:40 -0400

    test: update scratchpad to create mainnet wallet from seed

--- src/test/Scratchpad.ts
+++ src/test/Scratchpad.ts
@@ -1,57 +1,64 @@
 import TestUtils from "./utils/TestUtils";
 import WalletSyncPrinter from "./utils/WalletSyncPrinter";
-import {connectToDaemonRpc,
+import {createWalletFull,
         connectToWalletRpc,
-        createWalletFull,
-        MoneroNetworkType} from "../../index";
+        connectToDaemonRpc,
+        GenUtils,
+        MoneroNetworkType,
+        MoneroWalletListener} from "../../index";
 
 describe("Scratchpad", function() {
-  
+
   it("Can be scripted easily", async function() {
-    
+
 //    let daemon = await TestUtils.getDaemonRpc();
 //    let walletRpc = await TestUtils.getWalletRpc();
 //    let walletFull = await TestUtils.getWalletFull();
-    
-    // initialize daemon rpc client
+
+  //  // initialize daemon rpc client
+  //  let daemon = await connectToDaemonRpc({
+  //    uri: "http://localhost:28081",
+  //    username: "superuser",
+  //    password: "abctesting123",
+  //    proxyToWorker: TestUtils.PROXY_TO_WORKER,
+  //    rejectUnauthorized: false
+  //  });
+  //  console.log("Daemon height: " + await daemon.getHeight());
+
+  //  // initialize wallet rpc client
+  //  let walletRpc = await connectToWalletRpc({
+  //    uri: "http://localhost:28084",
+  //    username: "rpc_user",
+  //    password: "abc123",
+  //    rejectUnauthorized: false
+  //  });
+  //  await walletRpc.openWallet("test_wallet_1", "supersecretpassword123");
+  //  console.log("RPC wallet seed: " + await walletRpc.getSeed());
+
+    // create wallet from seed on mainnet
     let daemon = await connectToDaemonRpc({
-      uri: "http://localhost:28081",
-      username: "superuser",
-      password: "abctesting123",
-      proxyToWorker: TestUtils.PROXY_TO_WORKER,
+      uri: "http://xmr-node.cakewallet.com:18081",
       rejectUnauthorized: false
     });
-    console.log("Daemon height: " + await daemon.getHeight());
-    
-    // initialize wallet rpc client
-    let walletRpc = await connectToWalletRpc({
-      uri: "http://localhost:28084",
-      username: "rpc_user",
-      password: "abc123",
-      rejectUnauthorized: false
-    });
-    await walletRpc.openWallet("test_wallet_1", "supersecretpassword123");
-    console.log("RPC wallet seed: " + await walletRpc.getSeed());
-    
-    // create in-memory wallet with mnemonic
     let walletFull = await createWalletFull({
-      //path: "./test_wallets/" + GenUtils.getUUID(), // in-memory wallet if not given
-      password: "supersecretpassword123",
-      networkType: MoneroNetworkType.TESTNET,
+      path: "./test_wallets/" + GenUtils.getUUID(), // in-memory wallet if not given
+      password: "abctesting123",
+      networkType: MoneroNetworkType.MAINNET,
       server: {
-        uri: "http://localhost:28081",
-        username: "superuser",
-        password: "abctesting123",
-        rejectUnauthorized: false
+        uri: "http://xmr-node.cakewallet.com:18081",
+        //proxyUri: "socks5h://127.0.0.1:59787"
       },
-      seed: "silk mocked cucumber lettuce hope adrenalin aching lush roles fuel revamp baptism wrist long tender teardrop midst pastry pigment equip frying inbound pinched ravine frying",
-      restoreHeight: 0,
-      proxyToWorker: TestUtils.PROXY_TO_WORKER, 
+      restoreHeight: await daemon.getHeight() - 1000,
+      seed: "inbound boldly fuselage jukebox unveil rounded village summon swiftly aside shuffled rising examine friendly goat rockets girth mugged january yesterday went dented amnesty awful unveil"
     });
-    await walletFull.sync(new WalletSyncPrinter());
+    await walletFull.sync(new class extends MoneroWalletListener {
+      async onSyncProgress(height: number, startHeight: number, endHeight: number, percentDone: number, message: string) {
+        console.log("Sync progress: " + percentDone * 100 + "%, height: " + height + ", startHeight: " + startHeight + ", endHeight: " + endHeight + ", message: " + message);
+      }
+    });
     console.log("Full wallet daemon height: " + await walletFull.getDaemonHeight());
     console.log("Full wallet seed: " + await walletFull.getSeed());
-    console.log("Wallet balance: " + (await walletFull.getUnlockedBalance()).toString()) 
+    console.log("Wallet balance: " + (await walletFull.getUnlockedBalance()).toString())
     await walletFull.close();
   });
 });
